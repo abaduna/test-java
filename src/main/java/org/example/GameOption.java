a755f4c8a1ff2af0844353fbddff6fedd948aa2a
abaduna <artuelrey@gmail.com>
2025-01-22 17:29:20 -0300

    testing

---
+++ src/main/java/org/example/GameOption.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public enum GameOption {
+    ROCK, PAPER, SCISSORS;
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        double salary = 1000;
-        System.out.println(MoneyUtils.format(salary));
+       Game game = new Game();
+       game.play();
     }
 }
